55704fd0b2ca3e86298f40f2d567eaf9c5798880
Willson Smith <me@willsonsmith.com>
2016-04-04 19:16:46 -0400

    start refactor of js

--- public_assets/js/builder.js
+++ public_assets/js/builder.js
@@ -1,55 +1,32 @@
-var builder = (function(){
+var builder = (function() {
 
   return{
 
-    checkHash: function(callback){
+    checkHash: function(callback) {
       var answer,
           parts = location.hash.split('/'),
           data;
 
-      if (parts[0] === '#!'){
+      if (parts[0] === '#!') {
 
          answer = confirm("Are you sure you want clear all current movies and add this new set?");
-         if (answer){
-
-            //parts = location.hash.split('/');
-
+         if (answer) {
             if (parts[0] === '#!'){
               data = decodeURI(location.hash.substr(location.hash.indexOf("/") + 1));
-
-
-              //parts[1];
-              //console.log(JSON.parse(data));
-
               localforage.setItem('favourites', JSON.parse(data), function(data){
-
-
                 location.hash = '';
-
                 callback();
-
-
               });
-
-
-
             }
-
           } else{
-
             callback();
-
           }
-
         } else {
-
           callback();
-
         }
-
     },
 
-    setData: function(element, data){
+    setData: function(element, data) {
 
       var link = element.querySelector('.simLink'),
           elTitle = element.querySelector('.title'),
@@ -67,13 +44,10 @@ var builder = (function(){
           img.src = imageBaseURL + image;
           link.appendChild(img);
           link.setAttribute('href', '/similar/' + id);
-
-
           elFav.setAttribute('data-film', film);
           elFav.setAttribute('data-id', id);
           elFav.setAttribute('data-rating', rating);
           elFav.setAttribute('data-image', image);
-
           elTitle.innerHTML = film;
           elRating.innerHTML = rating;
 
@@ -81,7 +55,7 @@ var builder = (function(){
 
     },
 
-    makeResult: function(element){
+    makeResult: function (element) {
       var dupe = element.cloneNode(true),
           second,
           secondImg,
@@ -89,39 +63,18 @@ var builder = (function(){
 
       var applyTo = document.getElementById('resultId');
 
-
-      builder.checkHash(function(){
-
-      localforage.getItem('favourites', function(val){
-
-
+      builder.checkHash(function() {
+      localforage.getItem('favourites', function(val) {
         var firstResult = document.querySelector('.result'),
             data;
 
-        if(val){
+        if (val) {
           data = val.extraData;
         }
 
         document.getElementById('resultId').removeChild(element);
 
-        //});
-        //builder.setData(firstResult, data[0]);
-
-
-        //console.log(val);
-        /*for (var i = 1, l = data.length; i < l; i++) {
-
-          if (val.extraData[i].title !== '') {
-
-            second = dupe.cloneNode(true);
-
-            builder.setData(second, data[i]);
-            toAppend.appendChild(second);
-
-        }
-
-      }*/
-        for(var index in data) {
+        for (var index in data) {
           if (data.hasOwnProperty(index)) {
             var attr = data[index];
 
@@ -138,15 +91,13 @@ var builder = (function(){
 
       });
     });
-
     },
 
     prepEvents: function(){
-
       document.getElementById('deleteData').addEventListener('click', function(){
         var answer = confirm("Are you sure you want to remove all movies?");
 
-        if (answer){
+        if (answer) {
           localforage.removeItem('favourites').then(function(){
             document.location.reload(true);
           });
@@ -155,29 +106,18 @@ var builder = (function(){
       }, false);
 
       document.getElementById('backupMovies').addEventListener('click', function(){
-
-        localforage.getItem('favourites', function(val){
-
+        localforage.getItem('favourites', function(val) {
           var firstResult = document.querySelector('.result'),
               data;
 
-
-          if(val){
+          if (val) {
             val = JSON.stringify(val);
-            //console.log(JSON.stringify(val));
-
-            //location.hash='!/' + val;
             document.getElementById('textareaLink').value = 'http://similarfilms.com/mine/#!/' + encodeURI(val);
             location.hash = 'textareaLink';
-
           }
-
         });
-
       }, false);
-
     }
-
   };
 
 })();
@@ -185,4 +125,3 @@ var builder = (function(){
 
 builder.prepEvents();
 builder.makeResult(document.querySelector('.result'));
-
