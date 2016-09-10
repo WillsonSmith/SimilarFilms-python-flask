6f9fd6fcaf849d308ecae9271bee1a4cc3769c05
Willson Smith <me@willsonsmith.com>
2016-09-10 15:39:43 -0400

    better layout using a stack component

--- modules/get_data.py
+++ modules/get_data.py
@@ -1,8 +1,17 @@
 """module used by SimilarFilms to retrieve data"""
 import requests
 
+def chunks(array, number):
+    return [array[i:i+number] for i in range(0, len(array), number)]
+
+def getResults(response):
+    return response.get('results')
+
+def chunkResults(results):
+    return chunks(chunks(results, 2), 2)
+
 def get_data(path=""):
     """does the data fetch for the movie database json endpoint"""
     host = "api.themoviedb.org"
     response = requests.get("https://" + host + path).json
-    return response()
+    return chunkResults(getResults(response()))
